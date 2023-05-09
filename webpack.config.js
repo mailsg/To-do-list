28f8ff219dcc8a7f4ef33c359bc9ef12ad73f742
Sandeep Ghosh <mailsg0912@gmail.com>
2023-05-09 16:17:20 +0530

    Check the files with linter

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',  
-  
+  mode: 'development',
+
   entry: './src/index.js',
-  
+
   devServer: {
     static: './dist',
   },
-  
+
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
-  
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  
+
   module: {
     rules: [
       {
@@ -30,5 +30,5 @@ module.exports = {
       },
     ],
   },
-  
+
 };
