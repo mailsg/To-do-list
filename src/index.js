fa34cc60634e54835a351002c95c274d09875237
Sandeep Ghosh <mailsg0912@gmail.com>
2023-05-09 18:44:42 +0530

    list structure of the to do list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 /* eslint-disable no-use-before-define */
 import './style.css';
+
+const tasks = [
+  {
+    description: 'Learn HTML',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Learn CSS',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Learn JavaScript',
+    completed: false,
+    index: 0,
+  },
+];
+
+const viewTasks = () => {
+  const container = document.querySelector('.todo-list-placeholder');
+
+  tasks.sort((a, b) => a.index - b.index);
+
+  tasks.forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'list-item';
+    listItem.innerHTML = `<input class="check-box" type="checkbox">${task.description}`;
+    container.appendChild(listItem);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  viewTasks();
+});
