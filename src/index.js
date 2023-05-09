57907b5649f5ab7cd3c6473456ec5ec5ee7c309f
Sandeep Ghosh <mailsg0912@gmail.com>
2023-05-09 19:20:19 +0530

    Add remove tasks repo initialization

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 /* eslint-disable no-use-before-define */
 import './style.css';
 
-const tasks = [
-  {
-    description: 'Learn HTML',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Learn CSS',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Learn JavaScript',
-    completed: false,
-    index: 0,
-  },
-];
+// const tasks = [
+//   {
+//     description: 'Learn HTML',
+//     completed: false,
+//     index: 2,
+//   },
+//   {
+//     description: 'Learn CSS',
+//     completed: false,
+//     index: 1,
+//   },
+//   {
+//     description: 'Learn JavaScript',
+//     completed: false,
+//     index: 0,
+//   },
+// ];
 
 const viewTasks = () => {
   const container = document.querySelector('.todo-list-placeholder');
