e32f4787b35ed67d5a5987cdcd93bcafc3cab956
gisa chris <gisachris24@gmail.com>
2023-05-17 15:01:40 -0200

    adding describe functions

--- src/addRemove.test.js
+++ src/addRemove.test.js
@@ -1,18 +1,22 @@
 import taskManager from './addRemove.js';
 
 describe('taskManager', () => {
-  test('should add items to tasks based on user actions', () => {
-    const tasks = taskManager.addNewTask({
-      task: 'Task 1',
-      id: 1,
-      status: false,
+  describe('adding function test unit', () => {
+    test('should add items to tasks based on user actions', () => {
+      const tasks = taskManager.addNewTask({
+        task: 'Task 1',
+        id: 1,
+        status: false,
+      });
+      expect(tasks).not.toStrictEqual([]);
     });
-    expect(tasks).not.toStrictEqual([]);
   });
 
-  test('should remove appropriate task upon user actions', () => {
-    expect(
-      taskManager.removeTaskById(1),
-    ).toStrictEqual([]);
+  describe('subtraction function test unit', () => {
+    test('should remove appropriate task upon user actions', () => {
+      expect(
+        taskManager.removeTaskById(1),
+      ).toStrictEqual([]);
+    });
   });
 });
